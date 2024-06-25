bce5058c57e025b5357a5616f458dcfe33804bd5
thonghtph43253 <hoangthong2105@gmail.com>
2024-06-25 22:23:26 +0700

    Start

---
+++ code/FStoreShoe/src/main/java/org/example/fstoreshoe/FStoreShoeApplication.java
@@ -0,0 +1,13 @@
+package org.example.fstoreshoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FStoreShoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FStoreShoeApplication.class, args);
+	}
+
+}
---
+++ code/FStoreShoe/src/test/java/org/example/fstoreshoe/FStoreShoeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fstoreshoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FStoreShoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
